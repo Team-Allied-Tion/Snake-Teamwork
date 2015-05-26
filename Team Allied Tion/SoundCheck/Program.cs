d483d587894745fec3c596dee7704975f44faf4a
AsenTahchiyski <malkstor@gmail.com>
2015-05-26 18:07:54 +0300

    Teamwork in progress.

---
+++ Team Allied Tion/SoundCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using SoundCheck;
+
+class Program
+{
+    [DllImport("WMPLib.dll")]
+    static void Main()
+    {
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        wplayer.URL = "My MP3 file.mp3";
+        wplayer.controls.play();
+    }
+}
