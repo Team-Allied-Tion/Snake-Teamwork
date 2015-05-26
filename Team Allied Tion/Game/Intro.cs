ed65e172877a63800896a0549220764ebff59b18
donstany <donstany@abv.bg>
2015-05-26 18:45:41 +0300

    Add Intro

---
+++ Team Allied Tion/Game/Intro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01Intro
+{
+    public class Intro
+    {
+        static void PrintFile(string FileName)
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(FileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+            file.Close();
+            Thread.Sleep(1500);
+            Console.Clear();
+        }
+        public static void IntroAnimation()
+        {
+        
+            PrintFile("../../IntroHungry.txt");
+            PrintFile("../../IntroPython.txt");
+            PrintFile("../../IntroPythonAnimal.txt");
+            PrintFile("../../BeBlessed.txt");
+
+           
+            // Suspend the screen.
+
+            
+            Console.ReadLine();
+        }
+    }
+}
